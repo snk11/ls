93349db9bc1713d4d0de8508c579d68849ea65d1
Sulaiman YUSOF <syusof@e3r3p1.42.fr>
2015-10-15 21:12:44 +0200

    r

--- ft_ls.c
+++ ft_ls.c
@@ -6,8 +6,25 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/15 00:29:54 by syusof            #+#    #+#             */
-/*   Updated: 2015/10/15 00:30:11 by syusof           ###   ########.fr       */
+/*   Updated: 2015/10/15 21:08:26 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_ls.h"
+
+
+int main(int ac,char **av)
+{
+	struct dirent	*pdirent1;
+	DIR				*pdir1;
+
+	ac = 0;
+	pdir1 = opendir(av[0]);
+
+	while ((pdirent1 = readdir(pdir1)))
+	{
+		printf("%s\n",pdirent1->d_name);
+	}
+	closedir(pdir1);
+	return (0);
+}
--- ft_ls.h
+++ ft_ls.h
@@ -6,11 +6,35 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/15 00:30:22 by syusof            #+#    #+#             */
-/*   Updated: 2015/10/15 00:31:06 by syusof           ###   ########.fr       */
+/*   Updated: 2015/10/15 21:05:29 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef FT_LS_H
 # define FT_LS_H
+
+
+
+# include "libft/libft.h"
+# include <sys/stat.h>
+# include <dirent.h>
+# include <stdio.h>
+
+
+typedef struct		s_lst
+{
+	void			*content;
+	struct s_lst	*next;
+}					t_lst;
+
+
+typedef struct		s_stat
+{
+	char			*id;
+	struct stat		stat1;
+}					t_stat;
+
+
+
 
 # endif
