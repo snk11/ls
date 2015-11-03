4ac51ad3e58f0aabf983acfe211b0c35af9a1567
Sulaiman YUSOF <syusof@e3r4p6.42.fr>
2015-11-04 00:20:24 +0100

    r

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/15 00:29:54 by syusof            #+#    #+#             */
-/*   Updated: 2015/10/23 20:12:33 by syusof           ###   ########.fr       */
+/*   Updated: 2015/11/04 00:19:08 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,39 +20,42 @@ int main(int ac,char **av)
 	struct stat		sb;
 
 	ac = 0;
-	pdir1 = opendir(av[1]);
+	pdir1 = opendir(".");
 
 
-	stat(av[1], &sb);
-	{
-		if(S_ISDIR(sb.st_mode))
-			printf("d\n");
-		if(sb.st_mode & S_IRUSR)
-		{
-			printf("r\n");
-		}
-		printf("%d\n",sb.st_nlink);
-
-		printf("%s\n",getpwuid(sb.st_uid)->pw_name);
-		printf("%s\n",getgrgid(sb.st_gid)->gr_name);
-		printf("%lld\n",sb.st_size);
-		printf("%lld\n",sb.st_size);
-//		printf("%s\n",ctime(&(sb.st_ctime)));
-
-		printf("%d\n",(localtime(&(sb.st_ctime)))->tm_mon);
-		printf("%d\n",(localtime(&(sb.st_ctime)))->tm_mday);
-		printf("%d\n",(localtime(&(sb.st_ctime)))->tm_hour);
-		printf("%d\n",(localtime(&(sb.st_ctime)))->tm_min);
-		printf("%d\n",(localtime(&(sb.st_ctime)))->tm_year);
-	}
-
-	/*
 	while ((pdirent1 = readdir(pdir1)))
 	{
 		printf("%s\n",pdirent1->d_name);
 	}
 	closedir(pdir1);
-	return (0);
-*/
+	
+	if(av[1])
+//	if (strncmp(av[1], "-l",ft_strlen(av[1])))
+	{
+		stat(av[1], &sb);
+		{
+			if(S_ISDIR(sb.st_mode))
+				printf("d\n");
+			if(sb.st_mode & S_IRUSR)
+			{
+				printf("r\n");
+			}
+			printf("%d\n",sb.st_nlink);
 
+			printf("%s\n",getpwuid(sb.st_uid)->pw_name);
+			printf("%s\n",getgrgid(sb.st_gid)->gr_name);
+			printf("%lld\n",sb.st_size);
+			printf("%lld\n",sb.st_size);
+			//		printf("%s\n",ctime(&(sb.st_ctime)));
+
+			printf("%d\n",(localtime(&(sb.st_ctime)))->tm_mon);
+			printf("%d\n",(localtime(&(sb.st_ctime)))->tm_mday);
+			printf("%d\n",(localtime(&(sb.st_ctime)))->tm_hour);
+			printf("%d\n",(localtime(&(sb.st_ctime)))->tm_min);
+			printf("%d\n",(localtime(&(sb.st_ctime)))->tm_year);
+		}
+	}
+
+
+	return (0);
 }
