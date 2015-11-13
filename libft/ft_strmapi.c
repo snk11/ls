dcd1c2111423ce6294945de24ef22d22515b96a8
Sulaiman YUSOF <syusof@e3r3p1.42.fr>
2015-11-13 04:36:06 +0100

    r

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/06 10:54:27 by syusof            #+#    #+#             */
+/*   Updated: 2014/11/19 00:02:46 by syusof           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	if ((str = ft_strnew(ft_strlen(s) + 1)))
+	{
+		i = 0;
+		while (s && s[i] && f)
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
+		return (str);
+	}
+	return (NULL);
+}
