1c1cbf60b2da9974ef3c1d1833e63cf8538569ae
Sulaiman YUSOF <syusof@e3r8p1.42.fr>
2016-06-13 05:59:55 +0200

    r

--- ft_printerror.c
+++ ft_printerror.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/09 02:37:03 by syusof            #+#    #+#             */
-/*   Updated: 2016/06/13 05:47:40 by syusof           ###   ########.fr       */
+/*   Updated: 2016/06/13 05:59:30 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ void ft_printerror(char **av, t_ind *ind)
 				ft_putstr_fd("ls: ", 2);
 				perror(av[i]);
 			}
-			if (!ft_isdir(av[i]) && ft_isreg(av[i]))
+//			if (!ft_isdir(av[i]) && ft_isreg(av[i]))
+			if (!ft_isdir(av[i]))
 			{
 				lst = ft_create_lsto(av[i]);
 				lst1 = lst_addo_down_char(lst1,lst);
@@ -50,8 +51,13 @@ void ft_printerror(char **av, t_ind *ind)
 		lst1 = ft_lst_sort(lst1,croissant_char);
 	while (lst1)
 	{
-		ft_putstr(lst1->content);
-//		ft_putstr("\n");
+		char *s;
+		s = ft_memmove2(lst1->content);
+		if (ft_isreg(s))
+		{
+			ft_putstr(s);
+			ft_putstr("\n");
+		}
 		lst1 = lst1->next;
 	}
 	if (ind1 == 1)
