9a2ea5cb821b1dc2d8fc4509701fca4046bbbaf7
Sulaiman YUSOF <syusof@e2r7p5.42.fr>
2016-11-10 21:03:33 +0100

    r

--- ft_p0error2.c
+++ ft_p0error2.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 20:29:22 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/10 20:32:17 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/10 20:38:41 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,7 @@ void	ft_p0error_p1(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
 		inderror = 1;
 	}
 	if(inderror == 1 && !ft_isreg(ft_makepath(".",(char*)lst1->content)) && !ft_islnk(ft_makepath(".",(char*)lst1->content)))
-	{
-		ft_putstr_fd("ls: ", 2);
-		ft_putstr_fd((char*)(lst1->content), 2);
-		ft_putstr_fd(": ", 2);
-		ft_putstr_fd(strerror(errno),2);
-		ft_putstr_fd("\n", 2);
-		ind->inderror = 1;
-	}
+		ft_p0error_p1_p1(lst1, ind);
 	else if(ft_checkhyphencase(lstcmd,ind) == 1 &&  ind->indoption > 0 && ind->indexyet == 0 && ft_strcmp((char*)lst1->content,"--") == 0)
 	{
 		ind->indexyet = 1;
@@ -52,4 +45,14 @@ void	ft_p0error_p1(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
 			ind->inderror = 1;
 		}
 	}
+}
+
+void	ft_p0error_p1_p1(t_lsto *lst1,t_ind *ind)
+{
+		ft_putstr_fd("ls: ", 2);
+		ft_putstr_fd((char*)(lst1->content), 2);
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd(strerror(errno),2);
+		ft_putstr_fd("\n", 2);
+		ind->inderror = 1;
 }
