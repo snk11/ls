f0895e51a623a601b71d81ecf8388d620d0fd493
Sulaiman YUSOF <syusof@e3r5p3.42.fr>
2016-11-10 16:30:49 +0100

    r

--- ft_width.c
+++ ft_width.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/25 13:44:58 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/10 13:43:57 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/10 16:26:31 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,17 @@ void	ft_width(t_lsto *lst1,t_loption *loption)
 	gid = NULL;
 	while (lst1)
 	{
-//		if(ft_checkokfile(((t_rep*)(lst1->content))->name) == 1)
-//			ft_width2(lst1,loption);
-//		else
 	uid = NULL;
 	gid = NULL;
 		{
 			if (lstat(ft_makepath(((t_rep*)(lst1->content))->path,((t_rep*)(lst1->content))->name), &sb) == 0)
 			{
-					//uid = getpwuid(sb.st_uid);
-					//gid = getgrgid(sb.st_gid);
 				if(ft_strlen(ft_ustoa(sb.st_nlink)) > loption->link)
 					loption->link = ft_strlen(ft_ustoa(sb.st_nlink));
 				if(sb.st_uid && (uid = getpwuid(sb.st_uid)) != NULL)
 				{
-//					if(uid)
-					{
 						if(ft_strlen(uid->pw_name) > loption->uname)
 							loption->uname = ft_strlen(uid->pw_name);
-					}
 				}
 				else if(!(sb.st_uid))
 				{
@@ -50,11 +42,8 @@ void	ft_width(t_lsto *lst1,t_loption *loption)
 				}
 				if(sb.st_gid && (gid = getgrgid(sb.st_gid)) != NULL)
 				{
-//					if(gid)
-					{
 						if(ft_strlen(gid->gr_name) > loption->gname)
 							loption->gname = ft_strlen(gid->gr_name);
-					}
 				}
 				else if(!(sb.st_gid))
 				{
@@ -63,19 +52,10 @@ void	ft_width(t_lsto *lst1,t_loption *loption)
 				}
 				if(ft_strlen(ft_lldtoa(sb.st_size)) > loption->fsize)
 					loption->fsize = ft_strlen(ft_lldtoa(sb.st_size));
-				//			if(ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mon)) > loption->month)
-				//				loption->month = ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mon));
-				//			if(ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday)) > loption->day)
-				//				loption->day = ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday));
 				if(curtime - sb.st_mtime > 15778458)
-				{
 					loption->year = ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-				}
 				else if(curtime - sb.st_mtime < 0)
-				{
 					loption->year = ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-
-				}
 			}
 		}
 		lst1 = lst1->next;
@@ -93,30 +73,34 @@ void	ft_width2(t_lsto *lst1,t_loption *loption)
 
 	while (lst1)
 	{
-//		if(ft_checkokfile(((t_rep*)(lst1->content))->name) == 0)
-//			ft_width(lst1,loption);
-//		else
-		{
 			if(lstat(((t_rep*)(lst1->content))->name, &sb) == 0)
 			{
 				uid = getpwuid(sb.st_uid);
 				gid = getgrgid(sb.st_gid);
 				if(ft_strlen(ft_ustoa(sb.st_nlink)) > loption->link)
 					loption->link = ft_strlen(ft_ustoa(sb.st_nlink));
-				if((uid = getpwuid(sb.st_uid)) != NULL)
+				if(sb.st_uid && (uid = getpwuid(sb.st_uid)) != NULL)
 				{
-				if(ft_strlen(uid->pw_name) > loption->uname)
-					loption->uname = ft_strlen(uid->pw_name);
+						if(ft_strlen(uid->pw_name) > loption->uname)
+							loption->uname = ft_strlen(uid->pw_name);
 				}
-				if((gid = getgrgid(sb.st_gid)) != NULL)
+				else if(!(sb.st_uid))
 				{
-				if(ft_strlen(gid->gr_name) > loption->gname)
-					loption->gname = ft_strlen(gid->gr_name);
+					if(ft_strlen("root") > loption->uname)
+						loption->uname = 4;
+				}
+				if(sb.st_gid && (gid = getgrgid(sb.st_gid)) != NULL)
+				{
+						if(ft_strlen(gid->gr_name) > loption->gname)
+							loption->gname = ft_strlen(gid->gr_name);
+				}
+				else if(!(sb.st_gid))
+				{
+					if(ft_strlen("wheel") > loption->gname)
+						loption->gname = 5;
 				}
 				if(ft_strlen(ft_lldtoa(sb.st_size)) > loption->fsize)
 					loption->fsize = ft_strlen(ft_lldtoa(sb.st_size));
-				//			if(ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mon)) > loption->month)
-				//				loption->month = ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mon));
 				if(ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday)) > loption->day)
 					loption->day = ft_strlen(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday));
 				if(curtime - sb.st_mtime > 15778458)
@@ -129,7 +113,6 @@ void	ft_width2(t_lsto *lst1,t_loption *loption)
 
 				}
 			}
-		}
 		lst1 = lst1->next;
 	}
 
--- ft_wl.c
+++ ft_wl.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/25 14:27:40 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/10 13:44:13 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/10 16:30:13 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,123 +30,102 @@ void	ft_wl(t_lsto *lst1,t_loption loption)
 	linkname = NULL;
 	while (lst1)
 	{
-	uid = NULL;
-	gid = NULL;
-//		if(ft_checkokfile(((t_rep*)(lst1->content))->name))
-//			ft_wl2(lst1,loption);
-//		else
+		uid = NULL;
+		gid = NULL;
+		if (lstat(ft_makepath(((t_rep*)(lst1->content))->path,((t_rep*)(lst1->content))->name), &sb) == 0)
 		{
-			if (lstat(ft_makepath(((t_rep*)(lst1->content))->path,((t_rep*)(lst1->content))->name), &sb) == 0)
+			ft_print_permission(ft_makepath(((t_rep*)(lst1->content))->path,((t_rep*)(lst1->content))->name));
+			ft_putstr("  ");
+			ft_putwidth(ft_ustoa(sb.st_nlink),loption.link);
+			ft_putstr(" ");
+			if(sb.st_uid && (uid = getpwuid(sb.st_uid)) != NULL)
 			{
-				//uid = getpwuid(sb.st_uid);
-				//gid = getgrgid(sb.st_gid);
-				ft_print_permission(ft_makepath(((t_rep*)(lst1->content))->path,((t_rep*)(lst1->content))->name));
+				ft_putwidth(uid->pw_name,loption.uname);
 				ft_putstr("  ");
-				ft_putwidth(ft_ustoa(sb.st_nlink),loption.link);
+			}
+			else if(!(sb.st_uid))
+			{
+				ft_putwidth("root",loption.uname);
+				ft_putstr("  ");
+			}
+			if(sb.st_gid && (gid = getgrgid(sb.st_gid)) != NULL)
+			{
+				ft_putwidth(gid->gr_name,loption.gname);
+				ft_putstr("  ");
+			}
+			else if(!(sb.st_gid))
+			{
+				ft_putwidth("wheel",loption.gname);
+				ft_putstr("  ");
+			}
+			ft_putwidth(ft_lldtoa(sb.st_size),loption.fsize);
+			ft_putstr(" ");
+			ft_putmonth((localtime(&(sb.st_mtime)))->tm_mon);
+			ft_putstr(" ");
+			if(curtime - sb.st_mtime >= 0)
+			{
+				ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
 				ft_putstr(" ");
-				if(sb.st_uid && (uid = getpwuid(sb.st_uid)) != NULL)
-				{
-//					if(uid)
-						ft_putwidth(uid->pw_name,loption.uname);
-					ft_putstr("  ");
-				}
-				else if(!(sb.st_uid))
-				{
-					ft_putwidth("root",loption.uname);
-					ft_putstr("  ");
-				}
-				if(sb.st_gid && (gid = getgrgid(sb.st_gid)) != NULL)
-				{
-//					if(gid)
-						ft_putwidth(gid->gr_name,loption.gname);
-					ft_putstr("  ");
-				}
-				else if(!(sb.st_gid))
-				{
-					ft_putwidth("wheel",loption.gname);
-					ft_putstr("  ");
-				}
-				ft_putwidth(ft_lldtoa(sb.st_size),loption.fsize);
-				ft_putstr(" ");
-				ft_putmonth((localtime(&(sb.st_mtime)))->tm_mon);
-//				if(ft_strcmp(getgrgid(sb.st_gid)->gr_name,"syusof") != 0)
-					ft_putstr(" ");
-//				else
-//					ft_putstr("  ");
-
-				if(curtime - sb.st_mtime >= 0)
-				{
-					ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
-					ft_putstr(" ");
-				}
-				//			printf("time = %ld\n",curtime);
-				//			printf("time2 = %ld\n",sb.st_ctime);
-				//			printf("time3 = %ld\n",curtime-sb.st_ctime);
-				if(curtime - sb.st_mtime > 15778458)
-				{
-					if(loption.year > 5)
-						ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-					else
-					{
-						i = 5 - loption.year;
-						while (i > 0)
-						{
-							ft_putstr(" ");
-							i--;
-						}
-						ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-					}
-					ft_putstr(" ");
-				}
-				else if(curtime - sb.st_mtime < 0)
-				{
-					//ft_putstr(" ");
-					ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
-					ft_putstr(" ");
-					ft_putstr(" ");
+			}
+			if(curtime - sb.st_mtime > 15778458)
+			{
+				if(loption.year > 5)
 					ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-					ft_putstr(" ");
+				else
+				{
+					i = 5 - loption.year;
+					while (i > 0)
+					{
+						ft_putstr(" ");
+						i--;
+					}
+					ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
+				}
+				ft_putstr(" ");
+			}
+			else if(curtime - sb.st_mtime < 0)
+			{
+				ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
+				ft_putstr(" ");
+				ft_putstr(" ");
+				ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
+				ft_putstr(" ");
+			}
+			else
+			{
+				if (loption.year > 5)
+				{
+					i = loption.year - 5;
+					while(i > 0)
+					{
+						ft_putstr(" ");
+						i--;
+					}
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
+					ft_putstr(":");
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
 				}
 				else
 				{
-					if (loption.year > 5)
-					{
-						i = loption.year - 5;
-						while(i > 0)
-						{
-							ft_putstr(" ");
-							i--;
-						}
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
-						ft_putstr(":");
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
-					}
-					else
-					{
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
-						ft_putstr(":");
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
-					}
-					ft_putstr(" ");
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
+					ft_putstr(":");
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
 				}
-				//			ft_putulongnbr(sb.st_blocks);
-				//			l = l + sb.st_blocks;
-				//			ft_putstr(" ");
-				ft_putstr(((t_rep*)lst1->content)->name);
-				if(ft_islnk(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name )))
-				{
-					ft_putstr(" -> ");
-					linkname = (char*)malloc(sb.st_size + 1);
-					r = readlink(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name),linkname,sb.st_size);
-
-					if(linkname)
-					{
-						linkname[r] = 0;
-						ft_putstr(linkname);
-					}
-				}
-				ft_putstr("\n");
+				ft_putstr(" ");
 			}
+			ft_putstr(((t_rep*)lst1->content)->name);
+			if(ft_islnk(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name )))
+			{
+				ft_putstr(" -> ");
+				linkname = (char*)malloc(sb.st_size + 1);
+				r = readlink(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name),linkname,sb.st_size);
+				if(linkname)
+				{
+					linkname[r] = 0;
+					ft_putstr(linkname);
+				}
+			}
+			ft_putstr("\n");
 		}
 		lst1 = lst1->next;
 	}
@@ -168,109 +147,103 @@ void	ft_wl2(t_lsto *lst1,t_loption loption)
 	linkname = NULL;
 	while (lst1)
 	{
-//		if(ft_checkokfile(((t_rep*)(lst1->content))->name) == 0)
-//			ft_wl(lst1,loption);
-//		else
+		if (lstat(((t_rep*)(lst1->content))->name, &sb) == 0)
 		{
-			if (lstat(((t_rep*)(lst1->content))->name, &sb) == 0)
+			uid = getpwuid(sb.st_uid);
+			gid = getgrgid(sb.st_gid);
+			ft_print_permission(((t_rep*)(lst1->content))->name);
+			ft_putstr("  ");
+			ft_putwidth(ft_ustoa(sb.st_nlink),loption.link);
+			ft_putstr(" ");
+			if(sb.st_uid && (uid = getpwuid(sb.st_uid)) != NULL)
 			{
-				uid = getpwuid(sb.st_uid);
-				gid = getgrgid(sb.st_gid);
-				ft_print_permission(((t_rep*)(lst1->content))->name);
-				ft_putstr("  ");
-				ft_putwidth(ft_ustoa(sb.st_nlink),loption.link);
-				ft_putstr(" ");
-				if((uid = getpwuid(sb.st_uid)) != NULL)
-				{
 				ft_putwidth(uid->pw_name,loption.uname);
 				ft_putstr("  ");
-				}
-				if((gid = getgrgid(sb.st_gid)) != NULL)
-				{
+			}
+			else if(!(sb.st_uid))
+			{
+				ft_putwidth("root",loption.uname);
+				ft_putstr("  ");
+			}
+			if(sb.st_gid && (gid = getgrgid(sb.st_gid)) != NULL)
+			{
 				ft_putwidth(gid->gr_name,loption.gname);
 				ft_putstr("  ");
-				}
-				ft_putwidth(ft_lldtoa(sb.st_size),loption.fsize);
+			}
+			else if(!(sb.st_gid))
+			{
+				ft_putwidth("wheel",loption.gname);
+				ft_putstr("  ");
+			}
+			ft_putwidth(ft_lldtoa(sb.st_size),loption.fsize);
+			ft_putstr(" ");
+			ft_putmonth((localtime(&(sb.st_mtime)))->tm_mon);
+			ft_putstr(" ");
+			if(curtime - sb.st_mtime >= 0)
+			{
+				ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
 				ft_putstr(" ");
-				ft_putmonth((localtime(&(sb.st_mtime)))->tm_mon);
-//				if(ft_strcmp(getgrgid(sb.st_gid)->gr_name,"syusof") != 0)
-					ft_putstr(" ");
-//				else
-//					ft_putstr("  ");
-
-				if(curtime - sb.st_mtime >= 0)
-				{
-					ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
-					ft_putstr(" ");
-				}
-				//			printf("time = %ld\n",curtime);
-				//			printf("time2 = %ld\n",sb.st_ctime);
-				//			printf("time3 = %ld\n",curtime-sb.st_ctime);
-				if(curtime - sb.st_mtime > 15778458)
-				{
-					if(loption.year > 5)
-						ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-					else
-					{
-						i = 5 - loption.year;
-						while (i > 0)
-						{
-							ft_putstr(" ");
-							i--;
-						}
-						ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-					}
-					ft_putstr(" ");
-				}
-				else if(curtime - sb.st_mtime < 0)
-				{
-				//	ft_putstr(" ");
-					ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
-					ft_putstr(" ");
-					ft_putstr(" ");
+			}
+			if(curtime - sb.st_mtime > 15778458)
+			{
+				if(loption.year > 5)
 					ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
-					ft_putstr(" ");
+				else
+				{
+					i = 5 - loption.year;
+					while (i > 0)
+					{
+						ft_putstr(" ");
+						i--;
+					}
+					ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
+				}
+				ft_putstr(" ");
+			}
+			else if(curtime - sb.st_mtime < 0)
+			{
+				ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday),loption.day);
+				ft_putstr(" ");
+				ft_putstr(" ");
+				ft_putstr(ft_itoa((localtime(&(sb.st_mtime)))->tm_year + 1900));
+				ft_putstr(" ");
+			}
+			else
+			{
+				if (loption.year > 5)
+				{
+					i = loption.year - 5;
+					while(i > 0)
+					{
+						ft_putstr(" ");
+						i--;
+					}
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
+					ft_putstr(":");
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
 				}
 				else
 				{
-					if (loption.year > 5)
-					{
-						i = loption.year - 5;
-						while(i > 0)
-						{
-							ft_putstr(" ");
-							i--;
-						}
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
-						ft_putstr(":");
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
-					}
-					else
-					{
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
-						ft_putstr(":");
-						ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
-					}
-					ft_putstr(" ");
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_hour),loption.hour);
+					ft_putstr(":");
+					ft_putwidth2(ft_itoa((localtime(&(sb.st_mtime)))->tm_min),loption.minute);
 				}
-				//			ft_putulongnbr(sb.st_blocks);
-				//			l = l + sb.st_blocks;
-				//			ft_putstr(" ");
-				ft_putstr(((t_rep*)lst1->content)->name);
-				if(ft_islnk(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name )))
-				{
-					ft_putstr(" -> ");
-					linkname = (char*)malloc(sb.st_size + 1);
-					r = readlink(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name),linkname,sb.st_size);
-
-					if(linkname)
-					{
-						linkname[r] = 0;
-						ft_putstr(linkname);
-					}
-				}
-				ft_putstr("\n");
+				ft_putstr(" ");
 			}
+			ft_putstr(((t_rep*)lst1->content)->name);
+			if(ft_islnk(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name )))
+			{
+				ft_putstr(" -> ");
+				linkname = (char*)malloc(sb.st_size + 1);
+				r = readlink(ft_makepath(((t_rep*)lst1->content)->path,((t_rep*)lst1->content)->name),linkname,sb.st_size);
+
+				if(linkname)
+				{
+					linkname[r] = 0;
+					ft_putstr(linkname);
+				}
+			}
+			ft_putstr("\n");
 		}
 		lst1 = lst1->next;
 	}
