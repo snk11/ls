3ad7116e3f84f0a342c06997bdb749f15203f292
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-11-13 07:33:01 +0100

    r

--- ft_p_illegal.c
+++ ft_p_illegal.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/19 17:38:49 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/10 16:14:02 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 07:32:25 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	ft_p_illegal(t_ind *ind)
 		ft_putstr_fd("illegal option -- ", 2);
 		ft_putchar_fd(ind->indillegal, 2);
 		ft_putstr_fd("\n", 2);
-		ft_putstr_fd("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n",2);
+		ft_putstr_fd("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] ", 2);
+		ft_putstr_fd("[file ...]\n", 2);
 		ind->ind1 = 1;
 		ind->inderror = 1;
 		return (0);
 	}
 	return (1);
-
 }
