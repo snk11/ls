dede09b97fc567c6d6e80784790a177d7409dfbd
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-11-13 07:21:59 +0100

    r

--- ft_p0error.c
+++ ft_p0error.c
@@ -6,23 +6,23 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 15:31:27 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/11 10:16:39 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 07:16:30 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	ft_p0error(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
+void	ft_p0error(t_lsto *lst1, t_ind *ind, t_lsto *lstcmd)
 {
 	ind->indexyet = 0;
-	while(lst1 && ind->indfts == 0)
+	while (lst1 && ind->indfts == 0)
 	{
-		if(ft_strcmp(((char*)lst1->content),"") == 0)
+		if (ft_strcmp(((char*)lst1->content), "") == 0)
 		{
 			ind->indfts = 1;
 			opendir((char*)(lst1->content));
 			ft_putstr_fd("ls: fts_open: ", 2);
-			ft_putstr_fd(strerror(errno),2);
+			ft_putstr_fd(strerror(errno), 2);
 			ft_putstr_fd("\n", 2);
 			ind->inderror = 1;
 		}
--- ft_p0error2.c
+++ ft_p0error2.c
@@ -6,13 +6,13 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 20:29:22 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/11 10:19:48 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 07:20:41 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	ft_p0error_p1(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
+void	ft_p0error_p1(t_lsto *lst1, t_ind *ind, t_lsto *lstcmd)
 {
 	int		inderror;
 	char	*s1;
@@ -23,39 +23,43 @@ void	ft_p0error_p1(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
 	{
 		inderror = 1;
 	}
-	if(inderror == 1 && !ft_isreg(ft_makepath(".",(char*)lst1->content)) && !ft_islnk(ft_makepath(".",(char*)lst1->content)))
+	if (inderror == 1 && !ft_isreg(ft_makepath(".", (char*)lst1->content))
+			&& !ft_islnk(ft_makepath(".", (char*)lst1->content)))
 		ft_p0error_p1_p1(lst1, ind);
-	else if(ft_checkhyphencase(lstcmd,ind) == 1 &&  ind->indoption > 0 && ind->indexyet == 0 && ft_strcmp((char*)lst1->content,"--") == 0)
-	{
+	else if (ft_checkhyphencase(lstcmd, ind) == 1 && ind->indoption > 0
+			&& ind->indexyet == 0 && ft_strcmp((char*)lst1->content, "--") == 0)
 		ind->indexyet = 1;
-	}
-	else if(inderror == 1 && !ft_isreg(ft_makepath(".",(char*)lst1->content)) && !ft_islnk((ft_makepath(".",(char*)lst1->content))))
+	else if (inderror == 1 && !ft_isreg(ft_makepath(".", (char*)lst1->content))
+			&& !ft_islnk((ft_makepath(".", (char*)lst1->content))))
 		ft_p0error_p1_p2(lst1, ind);
 }
 
-void	ft_p0error_p1_p1(t_lsto *lst1,t_ind *ind)
+void	ft_p0error_p1_p1(t_lsto *lst1, t_ind *ind)
 {
 	ft_putstr_fd("ls: ", 2);
 	ft_putstr_fd((char*)(lst1->content), 2);
 	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(strerror(errno),2);
+	ft_putstr_fd(strerror(errno), 2);
 	ft_putstr_fd("\n", 2);
 	ind->inderror = 1;
 }
 
-void	ft_p0error_p1_p2(t_lsto *lst1,t_ind *ind)
+void	ft_p0error_p1_p2(t_lsto *lst1, t_ind *ind)
 {
 	char	*s1;
 
 	s1 = ft_memmove2((char*)(lst1->content));
 	if (ind->ind1 == 0 && !ft_isreg(s1) && !ft_islnk(s1))
 	{
-		if ((!opendir((char*)(lst1->content)) && (((char*)(lst1->content))[0] != '-' || ind->indoption == 0 || ft_strcmp((char*)lst1->content,"--") == 0)))
+		if ((!opendir((char*)(lst1->content))
+					&& (((char*)(lst1->content))[0] != '-'
+						|| ind->indoption == 0
+						|| ft_strcmp((char*)lst1->content, "--") == 0)))
 		{
 			ft_putstr_fd("ls: ", 2);
 			ft_putstr_fd((char*)(lst1->content), 2);
 			ft_putstr_fd(": ", 2);
-			ft_putstr_fd(strerror(errno),2);
+			ft_putstr_fd(strerror(errno), 2);
 			ft_putstr_fd("\n", 2);
 		}
 		ind->inderror = 1;
