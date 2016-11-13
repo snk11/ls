f30d6f5ae9e6e47a032d1db66566df9e2490bce5
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-11-13 07:03:03 +0100

    r

--- ft_ls.c
+++ ft_ls.c
@@ -6,18 +6,18 @@
 /*   By: syusof <syusof@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/15 00:29:54 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/10 19:58:07 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 07:02:50 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_ls.h"
+#include "ft_ls.h"
 
-int main(int ac,char **av)
+int		main(int ac, char **av)
 {
 	t_lsto			*lstcmd;
 	t_lsto			*lst;
 	t_ind			*ind;
-	int j;
+	int				j;
 
 	lstcmd = NULL;
 	ind = NULL;
@@ -25,8 +25,8 @@ int main(int ac,char **av)
 	lst = NULL;
 	ft_ls_p2(av, &lstcmd);
 	ft_init(ind);
-	ft_scan_option(av,ind);
-	if (!av[1] || (ac == 2 && ft_strcmp(av[1],"--") == 0))
+	ft_scan_option(av, ind);
+	if (!av[1] || (ac == 2 && ft_strcmp(av[1], "--") == 0))
 	{
 		lst = ft_getreplist(".");
 		ft_printlist(lst);
