2b21c1d0252c7ae85b59b5c80fabae96a65fe52d
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-11-13 07:29:28 +0100

    r

--- ft_p0regfile.c
+++ ft_p0regfile.c
@@ -6,21 +6,20 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 15:14:46 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/11 10:35:41 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 07:29:12 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_ls.h"
+#include "ft_ls.h"
 
-
-void	ft_p0regfile(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
+void	ft_p0regfile(t_lsto *lst1, t_ind *ind, t_lsto *lstcmd)
 {
-	int	inderror;
+	int		inderror;
 	char	*s1;
 
 	ind->indexyet = 0;
 	s1 = NULL;
-	while(lst1)
+	while (lst1)
 	{
 		s1 = ft_memmove2((char*)(lst1->content));
 		inderror = 0;
@@ -28,57 +27,57 @@ void	ft_p0regfile(t_lsto *lst1,t_ind *ind,t_lsto *lstcmd)
 			if (!opendir((char*)(lst1->content)))
 				inderror = 1;
 		}
-		ind->index1 = ft_getindex(lst1,lstcmd);
-		if(ft_checkhyphencase(lstcmd,ind) == 1)
+		ind->index1 = ft_getindex(lst1, lstcmd);
+		if (ft_checkhyphencase(lstcmd, ind) == 1)
 			ft_p0regfile_p1(lst1, ind, s1, inderror);
-		else if(ft_checkhyphencase(lstcmd,ind) == 0)
+		else if (ft_checkhyphencase(lstcmd, ind) == 0)
 			ft_p0regfile_p2(lst1, ind, s1, inderror);
 		lst1 = lst1->next;
 	}
-
 }
 
-
-
-void	ft_p0regfile_p1(t_lsto *lst1, t_ind *ind, char *s1,int inderror)
+void	ft_p0regfile_p1(t_lsto *lst1, t_ind *ind, char *s1, int inderror)
 {
-	if(inderror == 1 && ind->indl == 1 && (ft_isreg(ft_makepath(".",(char*)(lst1->content))) || ft_islnk(ft_makepath(".",(char*)lst1->content))))
+	if (inderror == 1 && ind->indl == 1
+			&& (ft_isreg(ft_makepath(".", (char*)(lst1->content)))
+				|| ft_islnk(ft_makepath(".", (char*)lst1->content))))
 	{
 		ft_printlregfile(lst1);
 		ind->indregfile = 1;
 	}
-	else if(inderror == 1 && (ft_isreg(s1) || ft_islnk(s1))
-		   )			{
+	else if (inderror == 1 && (ft_isreg(s1) || ft_islnk(s1)))
+	{
 		ft_putstr((char*)(lst1->content));
 		ft_putstr("\n");
 		ind->indregfile = 1;
 	}
 }
 
-
-void	ft_p0regfile_p2(t_lsto *lst1, t_ind *ind, char *s1,int inderror)
+void	ft_p0regfile_p2(t_lsto *lst1, t_ind *ind, char *s1, int inderror)
 {
-	if(ind->index1 > 1 && inderror == 1 && ind->indl == 1 && (ft_isreg(ft_makepath(".",(char*)(lst1->content))) || ft_islnk(ft_makepath(".",s1))))
+	if (ind->index1 > 1 && inderror == 1 && ind->indl == 1
+			&& (ft_isreg(ft_makepath(".", (char*)(lst1->content)))
+				|| ft_islnk(ft_makepath(".", s1))))
 	{
 		ft_printlregfile(lst1);
 		ind->indregfile = 1;
 	}
-	else if(ind->index1 > 1 && inderror == 1 && (ft_isreg(s1) || ft_islnk(s1)))
+	else if (ind->index1 > 1 && inderror == 1 && (ft_isreg(s1) || ft_islnk(s1)))
 	{
 		ft_putstr((char*)(lst1->content));
 		ft_putstr("\n");
 		ind->indregfile = 1;
 	}
-	else if(inderror == 1 && ind->indl == 1 && (ft_isreg((char*)(lst1->content)) || ft_islnk(s1)))
+	else if (inderror == 1 && ind->indl == 1 &&
+			(ft_isreg((char*)(lst1->content)) || ft_islnk(s1)))
 	{
 		ft_printlregfile(lst1);
 		ind->indregfile = 1;
 	}
-	else if(inderror == 1 && (ft_isreg(s1) || ft_islnk(s1)))
+	else if (inderror == 1 && (ft_isreg(s1) || ft_islnk(s1)))
 	{
 		ft_putstr((char*)(lst1->content));
 		ft_putstr("\n");
 		ind->indregfile = 1;
 	}
-
 }
