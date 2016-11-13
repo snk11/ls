5587d4acfe6fa1561a1309fecfdea4114a41d3c6
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-11-13 17:28:48 +0100

    r

--- ft_wl1.c
+++ ft_wl1.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 11:07:37 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/13 17:20:48 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 17:25:13 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,9 @@ void	ft_wl_p3(t_loption loption, struct stat sb, time_t curtime)
 
 void	ft_wl_p4(t_loption loption, struct stat sb, time_t curtime)
 {
-//	ft_putwidth(ft_itoa((localtime(&(sb.st_mtime)))->tm_mday), loption.day);
 	ft_putwidth(ft_getday_str(ctime(&(sb.st_mtime))), loption.day);
-//	ft_putstr(ft_getday_str(ctime(&(sb.st_mtime))));
 	ft_putstr(" ");
 	ft_putstr(" ");
-//	ft_putstr(ctime(&(sb.st_mtime)));
-//	ft_putstr("\n");
 	ft_putstr(ft_getyear_str(ctime(&(sb.st_mtime))));
 	ft_putstr(" ");
 }
