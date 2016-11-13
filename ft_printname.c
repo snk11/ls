411c87e1051d36c0f08cc10fd4ea683b20b31b9d
Sulaiman YUSOF <syusof@e3r7p1.42.fr>
2016-11-13 08:59:09 +0100

    r

--- ft_printname.c
+++ ft_printname.c
@@ -6,22 +6,22 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 00:06:32 by syusof            #+#    #+#             */
-/*   Updated: 2016/11/13 08:53:04 by syusof           ###   ########.fr       */
+/*   Updated: 2016/11/13 08:58:37 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_ls.h"
+#include "ft_ls.h"
 
-void	ft_printname(char *s,t_lsto *lstcmd,t_ind *ind)
+void	ft_printname(char *s, t_lsto *lstcmd, t_ind *ind)
 {
 	int	cnt;
 
 	cnt = ft_countelem(lstcmd);
-	if (ft_checkhyphencase(lstcmd,ind) == 1 && ind->indhyphsolo == 0)
+	if (ft_checkhyphencase(lstcmd, ind) == 1 && ind->indhyphsolo == 0)
 		ft_printname_p1(s, lstcmd, ind, cnt);
-	else if(ft_checkhyphencase(lstcmd,ind) == 0 || ind->indhyphsolo == 1)
+	else if (ft_checkhyphencase(lstcmd, ind) == 0 || ind->indhyphsolo == 1)
 	{
-		if(ft_printname_p2(s, lstcmd, ind, cnt))
+		if (ft_printname_p2(s, lstcmd, ind, cnt))
 		{
 		}
 		else
@@ -29,44 +29,46 @@ void	ft_printname(char *s,t_lsto *lstcmd,t_ind *ind)
 	}
 }
 
-
-void	ft_printname_p1(char *s,t_lsto *lstcmd,t_ind *ind, int cnt)
+void	ft_printname_p1(char *s, t_lsto *lstcmd, t_ind *ind, int cnt)
 {
-
-	if(ft_checkhyphencase_print(lstcmd,ind) == 1)
+	if (ft_checkhyphencase_print(lstcmd, ind) == 1)
 	{
 	}
-	else if(ind->indposition == 1 && cnt < 4)
+	else if (ind->indposition == 1 && cnt < 4)
 	{
 	}
-	else if(ind->indposition == 1 && cnt >= 4)
+	else if (ind->indposition == 1 && cnt >= 4)
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
 	}
-	else if(ind->indposition >= 2)
+	else if (ind->indposition >= 2)
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
 	}
 }
 
-int		ft_printname_p2(char *s,t_lsto *lstcmd,t_ind *ind, int cnt)
+int		ft_printname_p2(char *s, t_lsto *lstcmd, t_ind *ind, int cnt)
 {
-	if(ind->indoption > 0 && (cnt - ind->indoption) >= 2)
+	if (ind->indoption > 0 && (cnt - ind->indoption) >= 2)
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
 		return (1);
 	}
-	else if((ind->index1 == 1 && ind->indoption == 0) || (ind->index1 == 0 && ind->indhyphsolo == 1) || ind->indexyet == 1)
+	else if ((ind->index1 == 1 && ind->indoption == 0)
+			|| (ind->index1 == 0 && ind->indhyphsolo == 1)
+			|| ind->indexyet == 1)
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
 		ind->indexyet = 1;
 		return (1);
 	}
-	else if(((ind->index1 == (2 + ind->indoption) && ft_checkhyphencase(lstcmd,ind) == 0) || ind->indexyet == 1) && ft_strcmp(s,"--") == 1)
+	else if (((ind->index1 == (2 + ind->indoption)
+					&& ft_checkhyphencase(lstcmd, ind) == 0)
+				|| ind->indexyet == 1) && ft_strcmp(s, "--") == 1)
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
@@ -76,21 +78,22 @@ int		ft_printname_p2(char *s,t_lsto *lstcmd,t_ind *ind, int cnt)
 	return (0);
 }
 
-
-void	ft_printname_p3(char *s,t_lsto *lstcmd,t_ind *ind)
+void	ft_printname_p3(char *s, t_lsto *lstcmd, t_ind *ind)
 {
-	if(((ind->index1 == (2 + ind->indoption) && ft_checkhyphencase(lstcmd,ind) == 0) || ind->indexyet4 == 1))
+	if (((ind->index1 == (2 + ind->indoption)
+					&& ft_checkhyphencase(lstcmd, ind) == 0)
+				|| ind->indexyet4 == 1))
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
 		ind->indexyet4 = 1;
 	}
-	else if(ind->index1 >= (2 + ind->indoption) )
+	else if (ind->index1 >= (2 + ind->indoption))
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
 	}
-	else if(ind->inderror == 1)
+	else if (ind->inderror == 1)
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
