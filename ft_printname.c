9d355b051fdb724492ff207a1969b7360d4eda39
Sulaiman YUSOF <syusof@e1r7p4.42.fr>
2017-02-15 13:36:42 +0100

    r

--- ft_printname.c
+++ ft_printname.c
@@ -6,7 +6,7 @@
 /*   By: syusof <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 00:06:32 by syusof            #+#    #+#             */
-/*   Updated: 2017/02/15 13:30:02 by syusof           ###   ########.fr       */
+/*   Updated: 2017/02/15 13:32:59 by syusof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,26 @@ void		ft_printname(char *s, t_lsto *lstcmd, t_ind *ind)
 	int	cnt;
 
 	cnt = ft_countelem(lstcmd);
+	if ((cnt - ind->indoption) >= 2)
+	{
+		ft_putstr(s);
+		ft_putstr(":\n");
+	}
 	/*
 	if (ft_checkhyphencase(lstcmd, ind) == 1 && ind->indhyphsolo == 0)
 	{
 		ft_printname_p1(s, lstcmd, ind, cnt);
 	}
 	else if (ft_checkhyphencase(lstcmd, ind) == 0 || ind->indhyphsolo == 1)
-	*/
 	{
 		if (ft_printname_p2(s, lstcmd, ind, cnt))
 			;
 		else
 			ft_printname_p3(s, lstcmd, ind);
 	}
+	*/
 }
-
+/*
 void		ft_printname_p1(char *s, t_lsto *lstcmd, t_ind *ind, int cnt)
 {
 	if (ft_checkhyphencase_print(lstcmd, ind) == 1)
@@ -54,13 +59,6 @@ void		ft_printname_p1(char *s, t_lsto *lstcmd, t_ind *ind, int cnt)
 
 int			ft_printname_p2(char *s, t_lsto *lstcmd, t_ind *ind, int cnt)
 {
-	if ((cnt - ind->indoption) >= 2)
-	{
-		ft_putstr(s);
-		ft_putstr(":\n");
-		return (1);
-	}
-	/*
 	if (ind->indoption > 0 && (cnt - ind->indoption) >= 2)
 	{
 		ft_putstr(s);
@@ -85,13 +83,11 @@ int			ft_printname_p2(char *s, t_lsto *lstcmd, t_ind *ind, int cnt)
 		ind->indexyet = 1;
 		return (1);
 	}
-	*/
 	return (0);
 }
 
 void		ft_printname_p3(char *s, t_lsto *lstcmd, t_ind *ind)
 {
-	/*
 	if (((ind->index1 == (2 + ind->indoption)
 					&& ft_checkhyphencase(lstcmd, ind) == 0)
 				|| ind->indexyet4 == 1))
@@ -100,8 +96,7 @@ void		ft_printname_p3(char *s, t_lsto *lstcmd, t_ind *ind)
 		ft_putstr(":\n");
 		ind->indexyet4 = 1;
 	}
-	*/
-	if (ind->index1 >= (2 + ind->indoption))
+	else if (ind->index1 >= (2 + ind->indoption))
 	{
 		ft_putstr(s);
 		ft_putstr(":\n");
@@ -112,3 +107,4 @@ void		ft_printname_p3(char *s, t_lsto *lstcmd, t_ind *ind)
 		ft_putstr(":\n");
 	}
 }
+*/
